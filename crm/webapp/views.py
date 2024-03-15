61201f08fccde478ea745e08199aee431d4432ee
xingr121 <xinghuang121@gmail.com>
2024-03-15 16:40:15 -0400

    html and view

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .forms import CreateUserForm,LoginForm
 # Create your views here.
+
+def home(request):
+  
+  return render(request,"webapp/index.html")
+
+
